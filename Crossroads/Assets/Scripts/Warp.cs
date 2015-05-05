26b01f93ea6d39bd92f3a965227ce7ff0b22a6c3
Jon B <jondbridges@gmail.com>
2015-05-05 18:23:22 -0500

    Adds audio to warping. Adds specific audio for using door.

--- Crossroads/Assets/Scripts/Warp.cs
+++ Crossroads/Assets/Scripts/Warp.cs
@@ -5,17 +5,27 @@ public class Warp : MonoBehaviour {
 
 	public Transform warpTarget;
 
+	public AudioClip exitAudio;
+	public AudioClip arrivalAudio;
+
 	IEnumerator OnTriggerEnter2D(Collider2D other) {
-
 		ScreenFader sf = GameObject.FindGameObjectWithTag("Fader").GetComponent<ScreenFader> ();
+		AudioSource audioSource = GetComponent<AudioSource> ();
 
+		audioSource.PlayOneShot(exitAudio);
 		yield return StartCoroutine (sf.FadeToBlack());
 
-		other.gameObject.transform.position = warpTarget.position;
+		warpToTargetLocation(other);
+
+		audioSource.PlayOneShot(arrivalAudio);
+		yield return StartCoroutine (sf.FadeToClear());
+	}
+
+	void warpToTargetLocation(Collider2D warpPassenger) {
+		warpPassenger.gameObject.transform.position = warpTarget.position;
+
 		Vector3 newCameraPosition = warpTarget.position;
 		newCameraPosition.z = Camera.main.transform.position.z;
 		Camera.main.transform.position = newCameraPosition;
-
-		yield return StartCoroutine (sf.FadeToClear());
 	}
 }
